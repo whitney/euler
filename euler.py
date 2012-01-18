330e3bee3004d6047e7a473724b12fdd771c6038
Whitney Zoller <whitney@wzllr.com>
2012-01-17 20:54:53 -0500

    soln for problem 14

--- euler.py
+++ euler.py
@@ -4,6 +4,7 @@
 import sys
 import time
 import math
+from operator import mul
 
 class Euler():
 
@@ -261,6 +262,190 @@ class P7(Euler):
 			nth_primes = self.sieve(init)
 
 		return nth_primes[10001-1]
+
+	def test(self):
+		start = time.time()
+		answer = self.soln0()
+		elapsed = (time.time() - start)
+		print "soln0: %s, time: %s" % (answer, elapsed)
+
+class P8(Euler):
+	"""TODO"""
+		
+	def soln0(self):
+		return 12
+
+	def test(self):
+		start = time.time()
+		answer = self.soln0()
+		elapsed = (time.time() - start)
+		print "soln0: %s, time: %s" % (answer, elapsed)
+
+class P9(Euler):
+	"""A Pythagorean triplet is a set of three natural numbers,
+	a < b < c, for which a^2 + b^2 = c^2
+
+	For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2
+
+	There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+	Find the product abc."""
+
+	def find_pyth_triplets(self, n):
+		"""Find solutions to: 
+			a^2 + b^2 = c^2
+			a + b + c = 1000
+		"""
+		for a in range(1, n):
+			for b in range(1, n):
+				sum = a*a + b*b
+				root = math.sqrt(sum)
+				if int(root) == root and (a + b + int(root) == n):
+					return a, b, int(root)
+
+	def soln0(self):
+		a, b, c = self.find_pyth_triplets(1000)
+		return a*b*c
+
+	def test(self):
+		start = time.time()
+		answer = self.soln0()
+		elapsed = (time.time() - start)
+		print "soln0: %s, time: %s" % (answer, elapsed)
+
+class P10(Euler):
+	"""Calculate the sum of all the primes below 2,000,000."""
+
+	def soln0(self):
+		return sum(self.sieve(2000000 - 1))
+
+	def test(self):
+		start = time.time()
+		answer = self.soln0()
+		elapsed = (time.time() - start)
+		print "soln0: %s, time: %s" % (answer, elapsed)
+
+class P11(Euler):
+	"""TODO"""
+		
+	def soln0(self):
+		return 12
+
+	def test(self):
+		start = time.time()
+		answer = self.soln0()
+		elapsed = (time.time() - start)
+		print "soln0: %s, time: %s" % (answer, elapsed)
+
+class P12(Euler):
+	"""The sequence of triangle numbers is generated 
+	by adding the natural numbers. So the 7th triangle 
+	number would be 1 + 2 + 3 + 4 + 5 + 6 + 7 = 28. 
+	The first ten terms would be 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+	
+	In general the nth term of the sequence is given by 1 + 2 + 3 ... + n.
+	
+	Let us list the factors of the first seven triangle numbers:
+	1: 1
+	3: 1, 3
+	6: 1, 2, 3, 6
+	10: 1, 2, 5, 10
+	15: 1, 3, 5, 15
+	21: 1, 3, 7, 21
+	28: 1, 2, 4, 7, 14, 28
+	
+	We can see that 28 is the first triangle number to have over 5 divisors.
+	
+	What is the value of the first triangle number to have over 500 divisors?
+	"""
+		
+	def gen_triangle_seq(self, n):
+		"""Returns the first n elements of the triangle sequence"""
+		seq = []
+		for i in range(1, n + 1):
+			seq.append(sum(range(1, i + 1)))
+		return seq
+		
+	def triangle_seq_soln(self, n):
+		"""Returns the first n elements of the triangle sequence"""
+		i = 1
+		cont = True
+		while cont:
+			tri_num = sum(range(1, i + 1))
+			if len(self.factors(tri_num)) > n:
+				return tri_num
+			i += 1
+
+	def soln0(self):
+		return self.triangle_seq_soln(500)
+
+	def test(self):
+		start = time.time()
+		answer = self.soln0()
+		elapsed = (time.time() - start)
+		print "soln0: %s, time: %s" % (answer, elapsed)
+
+class P13(Euler):
+	"""TODO"""
+
+	def soln0(self):
+		return 12
+
+	def test(self):
+		start = time.time()
+		answer = self.soln0()
+		elapsed = (time.time() - start)
+		print "soln0: %s, time: %s" % (answer, elapsed)
+
+class P14(Euler):
+	"""The following iterative sequence is defined for the set of positive integers:
+		n --> n/2 (n is even)
+		n --> 3n + 1 (n is odd)
+
+	Using the rule above and starting with 13, we generate the following sequence:
+		13 --> 40 --> 20 --> 10 --> 5 --> 16 --> 8 --> 4 --> 2 --> 1
+
+	It can be said that this sequence (starting at 13 and finishing at 1) contains 10
+	terms. although it has not been proved yet (Collatz problem), it is thought that all
+	starting numbers finish at 1.
+
+	Which starting number, under one million, produces the longest chain?
+
+	NOTE: Once the chain starts the terms are allowed to go above one million.
+	"""
+
+	def collatz_seq(self, start, cache={}):
+		seq = [start]
+		while start > 1:
+			if start % 2 == 0:
+				start = int(start/2)
+			else:
+				start = 3*start + 1
+
+			if cache.has_key(start):
+				# then we know the rest of the sequence
+				# so use it and return!
+				seq.extend(cache[start])
+				return seq
+			else:
+				seq.append(start)
+
+		return seq
+
+	def collatz_soln(self):
+		cache = {}
+		curr_best = 0
+		best_start = 1
+		for i in range(1, 1000000):
+			seq = self.collatz_seq(i, cache)
+			cache[i] = seq
+			if len(seq) > curr_best:
+				curr_best = len(seq)
+				best_start = i
+		return best_start
+
+	def soln0(self):
+		#return self.collatz_seq(837799)
+		return self.collatz_soln()
 
 	def test(self):
 		start = time.time()
