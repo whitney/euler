34830bbdb7002c35818f5fc1596b8972ac4ceef8
Whitney Zoller <whitney@wzllr.com>
2012-01-19 00:03:31 -0500

    soln for problem 11

--- euler.py
+++ euler.py
@@ -4,6 +4,7 @@
 import sys
 import time
 import math
+import numpy
 from operator import mul
 
 class Euler():
@@ -270,10 +271,43 @@ class P7(Euler):
 		print "soln0: %s, time: %s" % (answer, elapsed)
 
 class P8(Euler):
-	"""TODO"""
+	"""Find the greatest product of five consecutive digits in the 1000-digit number."""
+
+	number = "73167176531330624919225119674426574742355349194934"
+	number += "96983520312774506326239578318016984801869478851843"
+	number += "85861560789112949495459501737958331952853208805511"
+	number += "12540698747158523863050715693290963295227443043557"
+	number += "66896648950445244523161731856403098711121722383113"
+	number += "62229893423380308135336276614282806444486645238749"
+	number += "30358907296290491560440772390713810515859307960866"
+	number += "70172427121883998797908792274921901699720888093776"
+	number += "65727333001053367881220235421809751254540594752243"
+	number += "52584907711670556013604839586446706324415722155397"
+	number += "53697817977846174064955149290862569321978468622482"
+	number += "83972241375657056057490261407972968652414535100474"
+	number += "82166370484403199890008895243450658541227588666881"
+	number += "16427171479924442928230863465674813919123162824586"
+	number += "17866458359124566529476545682848912883142607690042"
+	number += "24219022671055626321111109370544217506941658960408"
+	number += "07198403850962455444362981230987879927244284909188"
+	number += "84580156166097919133875499200524063689912560717606"
+	number += "05886116467109405077541002256983155200055935729725"
+	number += "71636269561882670428252483600823257530420752963450"
 		
 	def soln0(self):
-		return 12
+		curr_best = 0
+		left_idx = 0
+		right_idx = 5
+		while right_idx <= len(self.number):
+			str_digits = self.number[left_idx:right_idx]
+			product = reduce(mul, [int(n) for n in str_digits])
+			if product > curr_best:
+				curr_best = product
+
+			left_idx += 1
+			right_idx += 1
+			
+		return curr_best
 
 	def test(self):
 		start = time.time()
@@ -325,10 +359,100 @@ class P10(Euler):
 		print "soln0: %s, time: %s" % (answer, elapsed)
 
 class P11(Euler):
-	"""TODO"""
+	"""In the 2020 grid below, four numbers along a diagonal 
+	line have been marked in red. The product of these numbers is:
+	 26 * 63 * 78 * 14 = 1788696.
+
+	What is the greatest product of four adjacent numbers in any 
+	direction (up, down, left, right, or diagonally) in the 2020 grid?
+	"""
+	
+	GRID = numpy.array([
+		[8,  2,  22, 97, 38, 15, 0,  40, 0,  75, 4,  5,  7,  78, 52, 12, 50, 77, 91, 8],
+		[49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4,  56, 62, 0],
+		[81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+		[52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
+		[22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+		[24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+		[32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+		[67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
+		[24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+		[21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
+		[78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
+		[16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
+		[86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+		[19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40],
+		[4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+		[88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+		[4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
+		[20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
+		[20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
+		[1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]])
+
+	COUNT = 3 # how many adjacent neighbors do we consider
+	
+	def walk_grid(self):
+		curr_best = 0
+		for row in range(len(self.GRID)):
+			for column in range(len(self.GRID[row])):
+				local_max = self.max_adjacent_product(row, column)
+				if local_max > curr_best:
+					curr_best = local_max
+		return curr_best
+			
+
+	def max_adjacent_product(self, row, column):
+		"""Computes the greatest product of the three
+		adjacent neighbors of cell (row, column) as well 
+		as cell (row, column).
+
+		NOTE: we need only examine the horizontal-left, 
+		up-left-diagonal, vertical, and up-right-diagonal
+		neighbors since we're guaranteed to examine the 
+		other positions by walking the grid AND we do not
+		want to duplicate work."""
+		anchor = self.GRID[row][column]
+		products = []
+
+		print "------- (row, column): (%s, %s) -------" % (row, column)
+		# horizontal-left
+		if column - self.COUNT >= 0:
+			neighbors = self.GRID[row][column - self.COUNT:column + 1]
+			#print "horizontal-left: %s" % neighbors
+			products.append(reduce(mul, neighbors))
+
+		# up-left-diagonal
+		if column - self.COUNT >= 0 and row - self.COUNT >= 0:
+			neighbors = []
+			for i in range(self.COUNT + 1):
+				neighbors.append(self.GRID[row - i][column - i])
+			#print "up-left-diagonal: %s" % neighbors
+			products.append(reduce(mul, neighbors))
+		
+		# verical
+		if row - self.COUNT >= 0:
+			col = self.GRID[:,column]
+			#print "column: %s" % col
+			neighbors = col[row - self.COUNT:row + 1]
+			#print "vertical: %s" % neighbors
+			products.append(reduce(mul, neighbors))					
+		
+		# up-right-diagonal
+		if (column + self.COUNT) <= (len(self.GRID[row]) - 1) and (row - self.COUNT >= 0):
+			neighbors = []
+			for i in range(self.COUNT + 1):
+				neighbors.append(self.GRID[row - i][column + i])
+			#print "up-right-diagonal: %s" % neighbors
+			products.append(reduce(mul, neighbors))
+
+		# if local products have been computed
+		# then return the max 
+		if products:
+			return max(products)
+		return 0
 		
 	def soln0(self):
-		return 12
+		return self.walk_grid()
 
 	def test(self):
 		start = time.time()
@@ -385,7 +509,8 @@ class P12(Euler):
 		print "soln0: %s, time: %s" % (answer, elapsed)
 
 class P13(Euler):
-	"""TODO"""
+	"""Work out the first ten digits of the sum of 
+	the following one-hundred 50-digit numbers."""
 
 	def soln0(self):
 		return 12
