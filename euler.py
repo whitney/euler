029b44b1359ece08cd586484bf929bd44d83d8b6
Whitney Zoller <whitney@wzllr.com>
2012-01-24 11:19:12 -0500

    meh

--- euler.py
+++ euler.py
@@ -808,6 +808,33 @@ class P18(Euler):
 		elapsed = (time.time() - start)
 		print "soln0: %s, time: %s" % (answer, elapsed)
 
+class P19(Euler):
+	"""You are given the following information, 
+	but you may prefer to do some research for yourself.
+
+		- 1 Jan 1900 was a Monday.
+		- Thirty days has September,
+		  April, June and November.
+		  All the rest have thirty-one,
+		  Saving February alone,
+		  Which has twenty-eight, rain or shine.
+		  And on leap years, twenty-nine.
+		- A leap year occurs on any year evenly divisible by 4, 
+		  but not on a century unless it is divisible by 400.
+
+	How many Sundays fell on the first of the month during 
+	the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+	"""
+
+	def soln0(self):
+		return 12
+
+	def test(self):
+		start = time.time()
+		answer = self.soln0()
+		elapsed = (time.time() - start)
+		print "soln0: %s, time: %s" % (answer, elapsed)
+
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		usg =  "usage: euler.py <problem-class>\n"
