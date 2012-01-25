974e99e19ae23fce8ed5e335be50daa13ef40f78
Whitney Zoller <whitney@wzllr.com>
2012-01-25 11:22:51 -0500

    p20 soln

--- euler.py
+++ euler.py
@@ -835,6 +835,29 @@ class P19(Euler):
 		elapsed = (time.time() - start)
 		print "soln0: %s, time: %s" % (answer, elapsed)
 
+class P20(Euler):
+	"""n! means n * (n  1)  ...  3  2  1
+
+	For example, 10! = 10 * 9  *...*  3 * 2 * 1 = 3628800,
+	and the sum of the digits in the number 10! is 
+	3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+	Find the sum of the digits in the number 100!
+	"""
+
+	def soln0(self):
+		fact = str(math.factorial(100))
+		sum = 0
+		for char in fact:
+			sum += int(char)
+		return sum
+
+	def test(self):
+		start = time.time()
+		answer = self.soln0()
+		elapsed = (time.time() - start)
+		print "soln0: %s, time: %s" % (answer, elapsed)
+
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		usg =  "usage: euler.py <problem-class>\n"
